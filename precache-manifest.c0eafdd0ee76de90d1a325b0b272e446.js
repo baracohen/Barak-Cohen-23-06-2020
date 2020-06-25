2cc3e2d135b97e4412549bc12b35929734bfc3cc
barak <barak1212127@gmail.com>
2020-06-25 13:08:26 +0300

    Updates

---
+++ precache-manifest.c0eafdd0ee76de90d1a325b0b272e446.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ea498a477bd927d5f557fa1f9c05a2",
+    "url": "/Barak-Cohen-23-06-2020/weather/index.html"
+  },
+  {
+    "revision": "8e651de741479a6f0b10",
+    "url": "/Barak-Cohen-23-06-2020/weather/static/css/main.df9d7ffa.chunk.css"
+  },
+  {
+    "revision": "c79634236f620b9bb67e",
+    "url": "/Barak-Cohen-23-06-2020/weather/static/js/2.fdc4ef1f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Barak-Cohen-23-06-2020/weather/static/js/2.fdc4ef1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e651de741479a6f0b10",
+    "url": "/Barak-Cohen-23-06-2020/weather/static/js/main.cb8c2f68.chunk.js"
+  },
+  {
+    "revision": "7338f3803eb46aaef197",
+    "url": "/Barak-Cohen-23-06-2020/weather/static/js/runtime-main.85fbed69.js"
+  },
+  {
+    "revision": "71e6cc865c3a861a15dfd03302ecfa63",
+    "url": "/Barak-Cohen-23-06-2020/weather/static/media/heroloLogo.71e6cc86.jpg"
+  },
+  {
+    "revision": "7322d5b92466db69b1d7f78558a84766",
+    "url": "/Barak-Cohen-23-06-2020/weather/static/media/weatherImage.7322d5b9.jpg"
+  }
+]);
